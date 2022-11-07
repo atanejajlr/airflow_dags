92881bb9ab947dee034cd6b395c40f91ded27887
atanejajlr <ataneja@jaguarlandrover.com>
2022-11-07 22:49:28 +0000

    completed upto cron exps

--- 12_cron_exps1.py
+++ 12_cron_exps1.py
@@ -3,6 +3,7 @@
 #2) What is a cron expression? A cron expression is a string comprising five fields separated by white space that
 # rpresentsa  set of times normally as a schedule to execute some routine
 #3) Preset cron expressions in airflow: None, @once, @hourly, @daily, @weekly, @monthly, @yearly
+#4) https://crontab.guru/
 
 from datetime import datetime, timedelta
 from airflow.operators.bash import BashOperator
@@ -19,9 +20,9 @@ default_args = {
 with DAG(
 
     default_args=default_args,
-    dag_id="dag_with_cron_expression",
+    dag_id="dag_with_cron_expression, running on Tuesdays and Friday's",
     start_date=datetime(2022, 11, 1),
-    schedule_interval='@daily'
+    schedule_interval='0 3 * * Tue,Fri' #3 AM on Tuesday's and Friday's, refer https://crontab.guru/#0_3_*_*_Tue,Fri
 
 
     
@@ -35,5 +36,3 @@ with DAG(
     bash_command="echo dag with cron expression"
 
     )
-
-
